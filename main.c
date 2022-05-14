bd90e7fb06bd9a7117af56fa4a7a6b8cdb2d5c80
pimium <pimium@gmail.com>
2022-05-14 11:52:39 +0200

    actual

--- main.c
+++ main.c
@@ -7,6 +7,35 @@
 
 #define OC0B_PIN PB1
 
+#define H 0x01
+#define G 0x02
+#define F 0x04
+#define E 0x08
+#define D 0x10
+#define C 0x20
+#define B 0x40
+#define A 0x80
+
+
+uint8_t hex[] ={
+    A|B|C|D|E|F,   // 0
+    B|C,           // 1
+    A|B|D|E|G,     // 2
+    A|B|C|D|G,     // 3
+    B|C|F|G,       // 4
+    A|C|D|F|G,     // 5
+    A|C|D|E|F|G,   // 6
+    A|B|C,         // 7
+    A|B|C|D|E|F|G, // 8
+    A|B|C|D|F|G,   // 9
+    A|B|C|E|F|G,   // a
+    C|D|E|F|G,     // b
+    A|D|E|F,       // c
+    B|C|D|E|G,     // d
+    A|D|E|F|G,     // e
+    A|E|F|G,       // f
+};
+
 static inline void initTimer0(void)
 {
     // Timer 0 configuration : Fast PWM
@@ -31,10 +60,10 @@ void write_byte(uint8_t byte_value){
         }else{
             PORTD &= ~(1 << DIO);
         }
-        for (volatile int j = 0; j < 0xff; ++j);
+        for (volatile int j = 0; j < 0x2f; ++j);
         PORTD |= (1 << CLK);
         byte_value = byte_value << 1;
-        for (volatile int j = 0; j < 0xff; ++j);
+        for (volatile int j = 0; j < 0x2f; ++j);
         PORTD &= ~(1 << CLK);
         
     }
@@ -59,15 +88,15 @@ int main(void)
     while (1){
         PORTD &= ~(1 << CS);
         PORTB &= ~(1 << PB0);
-        for (volatile int j = 0; j < 0xff; j++);
+        // for (volatile int j = 0; j < 0x1; j++);
         // write_byte((0x04));
         write_byte(1 + (a & 0x03));
-        for (volatile int j = 0; j < 0xff; j++);
-        write_byte(0x1 << (a & 0x7));
-        for (volatile int j = 0; j < 0xff; j++);
+        // for (volatile int j = 0; j < 0x1; j++);
+        write_byte(hex[0x0 + (a & 0x0f)]);
+        for (volatile int j = 0; j < 0x1f; j++);
         a++;
         PORTD |= (1 << CS);
         PORTB |= (1 << PB0);
-        for (volatile int j = 0; j < 0x8fff; j++);
+        for (volatile int32_t j = 0; j < 0x1ffff; j++);
     };
 }
